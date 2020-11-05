541fde98d66b25dcc03405c0bde9636f8a58ea73
OmariKing <omarikingdetroit@gmail.com>
2020-11-05 09:08:33 -0500

    Password reset form now asks for new password

--- LeftOvr/urls.py
+++ LeftOvr/urls.py
@@ -54,4 +54,8 @@ urlpatterns = [
          auth_views.PasswordResetConfirmView.as_view
          (template_name='password_reset_confirm.html'),
          name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view
+         (template_name='password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
