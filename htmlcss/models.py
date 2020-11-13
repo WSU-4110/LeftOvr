7723ee788263dce489e52ba1064466be0b5327ca
Susanna Arun <gg6080@wayne.edu>
2020-11-12 23:24:22 -0500

    Builder pattern for models.py

--- htmlcss/models.py
+++ htmlcss/models.py
@@ -9,14 +9,16 @@ def default_profile_image():
     return "profile_images/defaultprofilepic.png"
 
 class Customer(models.Model):
-    firstName       = models.CharField(max_length=30)
-    lastName        = models.CharField(max_length=30)
-    idNum           = models.CharField(verbose_name="idNum", max_length=13)
-    email           = models.EmailField(max_length=70, unique=True)
-    password       = models.CharField(max_length=30, unique=True, null=True)
-    date_joined     = models.DateTimeField(verbose_name="date joined", auto_now_add=True)
-    last_login      = models.DateTimeField(verbose_name="last login", auto_now=True)
-    custAddress     = models.CharField(max_length=90, null=True)
+    def __str__(self, port):
+        self.port=port
+        self.firstName  = models.CharField(max_length=30)
+        self.lastName        = models.CharField(max_length=30)
+        self.idNum           = models.CharField(verbose_name="idNum", max_length=13)
+        self.email           = models.EmailField(max_length=70, unique=True)
+        self.password       = models.CharField(max_length=30, unique=True, null=True)
+        self.date_joined     = models.DateTimeField(verbose_name="date joined", auto_now_add=True)
+        self.last_login      = models.DateTimeField(verbose_name="last login", auto_now=True)
+        self.custAddress     = models.CharField(max_length=90, null=True)
 
     is_admin        =models.BooleanField(default=False)
     is_active       =models.BooleanField(default=True)
@@ -26,48 +28,52 @@ class Customer(models.Model):
     #profile_image   =models.ImageField(max_length=255, upload_to=profile_Image, null=True, blank=True, default=default_profile_image)
     hide_email      =models.BooleanField(default=True)
 
-    def __str__(self):
-        return self.email
-
-    def profile_image_fileName(self):
-        return str(self.profile_image)[str(self.profile_image).index(f'profile_images/{self.pk}/'):]
+    def build(self):
+        return Port(self.email)
+    def profile_Image(self):
+        return Port(self.profile_image)[str(self.profile_image).index(f'profile_images/{self.pk}/'):]
 
 class Restaurant(models.Model):
-    busName = models.CharField(max_length=500)
-    busAddress = models.CharField(max_length=500)
-    einNum = models.CharField(max_length=9)
-    busEmail = models.CharField(max_length=750)
-    busPassWords = models.CharField(max_length=500)
+    def __str__(self, port):
+        self.busName = models.CharField(max_length=500)
+        self.busAddress = models.CharField(max_length=500)
+        self.einNum = models.CharField(max_length=9)
+        self.busEmail = models.CharField(max_length=750)
+        self.busPassWords = models.CharField(max_length=500)
 
-    def __str__(self):
-        return self.busName + '  ' + self.busAddress + '   ' + self.einNum
+    def build(self):
+        return Port(self.busName + '  ' + self.busAddress + '   ' + self.einNum+ '   '+self.busEmail+ '   '+self.busPassWords)
 
 class Meals(models.Model):
-    mealType = models.CharField(max_length=500)
-    mealAvail = models.CharField(max_length=1000)
+    def __str__(self, port):
+        self.mealType = models.CharField(max_length=500)
+        self.mealAvail = models.CharField(max_length=1000)
 
-    def __str__(self):
-        return self.mealAvail + ' ' + self.mealType
+    def build(self):
+        return Port(self.mealAvail + ' ' + self.mealType)
 
 class ContactUs(models.Model):
-    nameC = models.CharField(max_length=300)
-    emailC = models.CharField(max_length=500)
-    phoneC = models.CharField(max_length=13)
-    messageC = models.TextField(blank=True, null=True)
+    def __str__(self, port):
+        self.nameC = models.CharField(max_length=300)
+        self.emailC = models.CharField(max_length=500)
+        self.phoneC = models.CharField(max_length=13)
+        self.messageC = models.TextField(blank=True, null=True)
 
-    def __str__(self):
-        return self.nameC + ' ' + self.emailC + ' ' + self.phoneC + ' ' + self.messageC
+    def build(self):
+        return Port(self.nameC + ' ' + self.emailC + ' ' + self.phoneC + ' ' + self.messageC)
 
 class LocationArea(models.Model):
-    localArea = models.DecimalField(decimal_places=2, max_digits=10000)
-    localName = models.CharField(max_length=500)
+    def __str__(self, port):
+        self.localArea = models.DecimalField(decimal_places=2, max_digits=10000)
+        self.localName = models.CharField(max_length=500)
 
-    def __str__(self):
-        return self.localName + ' ' + self.localArea
+    def build(self):
+        return Port(self.localName + ' ' + self.localArea)
 
-class profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+class Profile(models.Model):
+    def __str__(self, port):
+        self.user = models.OneToOneField(User, on_delete=models.CASCADE)
+        self.image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
-    def __str__(self):
+    def build(self):
         return f'{self.user.username} Profile'
