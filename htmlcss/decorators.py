3f4df3eb39c694511b3cd772f217a814668f61c2
OmariKing <omarikingdetroit@gmail.com>
2020-12-03 10:56:27 -0500

    forms and decorators fix

--- htmlcss/decorators.py
+++ htmlcss/decorators.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import redirect
 
 
@@ -17,7 +16,29 @@ def allowed_users(allowed_roles=[]):
     def decorator(view_func):
         def wrapper_func(request, *args, **kwargs):
 
-            print('Working:', allowed_roles)
-            return view_func(request, *args, **kwargs)
+            group = None
+            if request.user.groups.exists():
+                group = request.user.groups.all()[0].name
+
+            if group in allowed_roles:
+                return view_func(request, *args, **kwargs)
+            else:
+                return redirect('index')
+
         return wrapper_func
     return decorator
+
+
+def restaurant_user(view_func):
+        def wrapper_function(request, *args, **kwargs):
+            group = None
+            if request.user.groups.exists():
+                group = request.user.groups.all()[0].name
+
+            if group == 'customer':
+                return redirect('index-customer')
+
+            if group == 'restaurant':
+                return view_func(request, *args, **kwargs)
+
+        return wrapper_function
