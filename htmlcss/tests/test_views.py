2688220b129361e788644f8e1204a8757da78870
OmariKing <omarikingdetroit@gmail.com>
2020-12-13 18:26:02 -0500

    Update Test Cases

--- htmlcss/tests/test_views.py
+++ htmlcss/tests/test_views.py
@@ -21,3 +21,44 @@ class TestViews(TestCase):
 
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'login.html')
+
+    def test_project_profile_page_get(self):
+        client = Client()
+
+        response = client.get(self.proPage)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'sampleUserProfile.html.html')
+
+    def test_project_register_get(self):
+        client = Client()
+
+        response = client.get(self.register)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'register.html')
+
+    def test_project_Leo_get(self):
+        client = Client()
+
+        response = client.get(self.LeoReview)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'LeosReview.html.html')
+
+    def test_project_Chip_get(self):
+        client = Client()
+
+        response = client.get(self.ChipReview)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'ChipotleReview.html')
+
+    def test_project_Dom_get(self):
+        client = Client()
+
+        response = client.get(self.DomReview)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'DominosReview.html')
+
