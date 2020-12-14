67e4cf062b410a2ae1bc7413d92f7ed993b32cd1
OmariKing <omarikingdetroit@gmail.com>
2020-12-13 20:44:56 -0500

    Update Test Cases

--- htmlcss/tests/test_urls.py
+++ htmlcss/tests/test_urls.py
@@ -14,27 +14,31 @@ class TestCase_HomePageURLs(TestCase):
         url= reverse('index')
         print (resolve(url))
         self.assertIn('<title>Home</title>', index)
-        self.assertEquals(resolve(url).func, index)
+        self.assertEquals(resolve(url).func.view_class, index)
 
     def test_about_is_resolved(self):
         url = reverse('about')
         print(resolve(url))
-        self.assertEquals(resolve(url).func, about)
+        self.assertEquals(resolve(url).func.view_class, about)
+
     def test_custReg_is_resolved(self):
         url = reverse('custReg')
         print(resolve(url))
-        self.assertEquals(resolve(url).func, custReg)
+        self.assertEquals(resolve(url).func.view_class, custReg)
+
     def test_why_is_resolved(self):
         url = reverse('wh')
         print(resolve(url))
-        self.assertEquals(resolve(url).func, wh)
+        self.assertEquals(resolve(url).func.view_class, wh)
         """fixed one issue in urls for unittest"""
+
     def test_cont_is_resolved(self):
         url = reverse('cont')
         print(resolve(url))
-        self.assertEquals(resolve(url).func, cont)
+        self.assertEquals(resolve(url).func.view_class, cont)
+
     def test_receipt_is_resolved(self):
         url = reverse('regRec')
-        f=resolve(url)
+        f = resolve(url)
         print(f)
-        self.assertEquals(f.func, regRec)
+        self.assertEquals(f.func.view_class, regRec)
